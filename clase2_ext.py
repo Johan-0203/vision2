702e8d97f773a963acb2d4e586d02bfc2d2f4a03
Johan-0203 <chaconjohan2003@gmail.com>
2023-02-08 11:23:57 -0500

    pdfs

--- clase2_ext.py
+++ clase2_ext.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import math
 
 W = 500
 H = 500
@@ -19,18 +20,21 @@ def esc(triangle,x,y):
     e = np.array([(x,0),(0,y)])
     return triangle @ e
 
-def rot(triangle,angle):
-    r = np.array([[np.cos(angle),-np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+def rot(triangle):
+    r = np.array([[np.cos(math.pi/2),-np.sin(math.pi/2)], [np.sin(math.pi/2), np.cos(math.pi/2)]])
     return triangle @ r
 
 
 im = np.zeros((W,H,3))
 triangle = np.array ([(10,10),(70,10),(40,60)])
-draw(triangle, im)
+# draw(triangle, im)
+# show_im(im)
 t1 = tras(triangle, 50,50)
-draw(t1,im, color=(0,0,255))
+# draw(t1,im, color=(0,0,255))
+# show_im(im)
 t2 = esc(t1,3,3)
-draw(t2,im, color=(255,0,0))
-t3 = rot(triangle,40)
+# draw(t2,im, color=(255,0,0))
+# show_im(im)
+t3 = rot(t2)
 draw(t3,im, color=(123,23,75))
 show_im(im)
